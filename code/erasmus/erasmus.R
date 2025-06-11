b82bc48ee511cad0b596c0cb695a23a6b181f673
olga-litvinova <litvinova2404ad@gmail.com>
2025-06-11 23:55:05 +0200

    commit

--- code/erasmus/erasmus.R
+++ code/erasmus/erasmus.R
@@ -7,6 +7,16 @@ library(dplyr)
 library(tidyr)
 library(ggplot2)
 library(readxl)
+library(tibble)
+install.packages("circlize")  
+
+library(circlize)
+
+install.packages("ggplotify")   
+library(ggplotify)
+install.packages("cowplot")
+
+library(cowplot)
 ## Import data
 setwd("C:\\Users\\Olga\\0. VS Code\\Github\\visualizations\\")
 df <- read_excel("data\\Erasmus-KA2-Mobility-Data.xlsx", sheet = "E+ KA2 Data (Since 2014)")
@@ -46,11 +56,45 @@ top_10_receiving<-df%>%
 top_countries<-unique(c(top_10_sending$sending_country_name, top_10_receiving$receiving_country_name))
 top_countries
 
-df<-df%>%
-  filter(sending_country_name %in% top_countries)%>%
-  filter(receiving_country_name %in% top_countries)%>%
+df_short<-df%>%
+  filter(sending_country_name %in% top_countries,
+         receiving_country_name %in% top_countries,
+         sending_country_name != receiving_country_name) %>%
   group_by(sending_country_name,receiving_country_name)%>%
-  filter(receiving_country_name != sending_country_name)%>%
-  summarise(amount=sum(`Actual Participants (Contracted Projects)`))%>%
+  summarise(amount = sum(`Actual Participants (Contracted Projects)`), .groups="drop") %>%
   arrange(desc(amount))
-  
+
+df_mat <- df %>%
+  filter(sending_country_name %in% top_countries,
+         receiving_country_name %in% top_countries,
+         sending_country_name != receiving_country_name) %>%
+  group_by(sending_country_name, receiving_country_name) %>%
+  summarise(amount = sum(`Actual Participants (Contracted Projects)`), .groups="drop") %>%
+  pivot_wider(names_from = receiving_country_name,
+              values_from = amount,
+              values_fill = 0) %>%
+  column_to_rownames("sending_country_name") %>%
+  as.matrix()
+
+pal <- c(
+  "#002765","#0061fd","#1cc6ff","#00b661","#5bf34d",
+  "#ffdd00","#ff7d00","#da2818","#ff006d","#8f00ff"
+)
+
+chordDiagram(df_short, grid.col = pal)
+
+p<-recordPlot()
+as.ggplot(ggdraw(p))+
+  labs(title="ERASMUS STUDENT MOBILITY",
+       subtitle="Movement of Erasmus participants among the leading countries (2014–2023)",
+       caption="Data from erasmus-plus | Created by Litvinova Olga")+
+  theme(text=element_text(family="Arial"),
+        plot.title=element_text(hjust=0.5, face="bold", size=20),
+        plot.subtitle=element_text(hjust=0.5, size=12, margin=margin(t=10)),
+        plot.caption=element_text(size=10, hjust=0.95, margin=margin(b=12)),
+        plot.margin   =margin(t=20))
+
+p
+
+
+
